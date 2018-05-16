17437a6045b8568cda6ffaf99f76ab04c95865a7
Davidlo0l <38057704+Davidlo0l@users.noreply.github.com>
2018-05-16 18:53:21 -0500

    Initial commit

---
+++ AppPropina/AppPropina/AppPropina/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPropina
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            btnCarcular.Clicked += BtnCarcular_Clicked; 
+		}
+
+        private void BtnCarcular_Clicked(object sender, EventArgs e)
+        {
+            var total = decimal.Parse(entTotal.Text);
+            var propina = decimal.Parse(entPropina.Text);
+            var numPersona = int.Parse(entPersonas.Text);
+            var totalneto = (total * propina) / 100;
+            TxtTotalPropina.Detail = totalneto.ToString("C");
+            TxtPropinaPersona.Detail = (totalneto / numPersona).ToString("C");
+            TxtTotal.Detail = (total + totalneto).ToString("C");
+            TxtTotalPersona.Detail = ((total+ totalneto)/ numPersona).ToString("C");
+
+
+
+        }
+    }
+}
